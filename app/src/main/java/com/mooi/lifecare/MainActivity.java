49d0a9ff2b4c8c469cfc95bcb2a1fae9c4a77b88
Amit <aniketmj.kumar@gmail.com>
2023-09-24 15:02:12 +0530

    database commit

--- app/src/main/java/com/mooi/lifecare/MainActivity.java
+++ app/src/main/java/com/mooi/lifecare/MainActivity.java
@@ -2,28 +2,64 @@ package com.mooi.lifecare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    TextView rgText;
+    EditText username,password;
+    Button loginBtn;
+    TextView registerHere;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        rgText = findViewById(R.id.registerTV);
+        username = findViewById(R.id.loginUsername);
+        password = findViewById(R.id.loginPassword);
+        loginBtn = findViewById(R.id.loginButton);
+        registerHere = findViewById(R.id.registerTV);
 
-        Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
-
-        rgText.setOnClickListener(new View.OnClickListener() {
+        registerHere.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
                 startActivity(intent);
             }
         });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName = username.getText().toString();
+                String passWord = password.getText().toString();
+
+                Database db = new Database(getApplicationContext(),"LifeCare",null,1);
+                if(userName.equals("") || passWord.equals("")){
+                    //do somthing
+                    Toast.makeText(MainActivity.this, "enter details", Toast.LENGTH_SHORT).show();
+                }else{
+                    if(db.login(userName,passWord) == 1){
+                        Toast.makeText(MainActivity.this, "login Success", Toast.LENGTH_SHORT).show();
+
+                    }else{
+                        Toast.makeText(MainActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                    }
+
+
+                    startActivity(new Intent(MainActivity.this,HomeActivity.class));
+
+                }
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/mooi/lifecare/RegisterActivity.java
+++ app/src/main/java/com/mooi/lifecare/RegisterActivity.java
@@ -2,13 +2,54 @@ package com.mooi.lifecare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    EditText username,password,confirmPsw;
+    Button regbtn;
+    TextView loginHere;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        username = findViewById(R.id.rgusername);
+        password = findViewById(R.id.rgpassword);
+        confirmPsw = findViewById(R.id.confirmPassword);
+        regbtn = findViewById(R.id.regbutton);
+        loginHere = findViewById(R.id.loginTv);
+
+        loginHere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this,MainActivity.class));
+            }
+        });
+
+        regbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName = username.getText().toString();
+                String passWord = password.getText().toString();
+                String cnfPsw = confirmPsw.getText().toString();
+
+                Database db = new Database(getApplicationContext(),"LifeCare",null,1);
+
+                if(userName.length() == 0 || passWord.length() == 0 || cnfPsw.length() == 0 ){
+
+                    Toast.makeText(RegisterActivity.this, "Enter all details", Toast.LENGTH_SHORT).show();
+                }else{
+                    db.register(userName,passWord);
+                    Toast.makeText(RegisterActivity.this, "record inserted", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
     }
 }
